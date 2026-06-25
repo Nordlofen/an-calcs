9410728de021d133b685091b9bfa5cceb2e0e79a
Anders Nordlöf <anders.nordlofen@gmail.com>
2026-06-25 18:50:18 +0200

    Remove obsolete calculation code

--- src/an_calcs/betong/palfundament/__init__.py
+++ src/an_calcs/betong/palfundament/__init__.py
@@ -1,8 +1,8 @@
 """Pålfundamentberäkningar för betong."""
 
-from ..fyrpalsfundament_teoretiskt import fyrpalsfundament_teoretiskt_innan_slagning
-from ..palsfundament import plot_palsfundament_3d, print_palsfundament_resultat
-from ..trepalsfundament_teoretiskt import trepalsfundament_teoretiskt_innan_slagning
+from .fyrpalsfundament_teoretiskt import fyrpalsfundament_teoretiskt_innan_slagning
+from .palsfundament import plot_palsfundament_3d, print_palsfundament_resultat
+from .trepalsfundament_teoretiskt import trepalsfundament_teoretiskt_innan_slagning
 
 __all__ = [
     "fyrpalsfundament_teoretiskt_innan_slagning",
