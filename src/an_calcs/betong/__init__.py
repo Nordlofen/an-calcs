7bc9e1e42a43147271531e20b226a61b87c8643e
Anders Nordlöf <anders.nordlofen@gmail.com>
2026-06-25 18:37:56 +0200

    Trim betong package exports

--- src/an_calcs/betong/__init__.py
+++ src/an_calcs/betong/__init__.py
@@ -2,26 +2,18 @@
 
 from .fyrpalsfundament_teoretiskt import (
     fyrpalsfundament_teoretiskt_innan_slagning,
-    plot_fyrpalsfundament_3d,
-    print_fyrpalsfundament_resultat,
 )
 from .palsfundament import (
     plot_palsfundament_3d,
     print_palsfundament_resultat,
 )
 from .trepalsfundament_teoretiskt import (
-    plot_trepalsfundament_3d,
-    print_trepalsfundament_resultat,
     trepalsfundament_teoretiskt_innan_slagning,
 )
 
 __all__ = [
     "fyrpalsfundament_teoretiskt_innan_slagning",
-    "plot_fyrpalsfundament_3d",
     "plot_palsfundament_3d",
-    "plot_trepalsfundament_3d",
-    "print_fyrpalsfundament_resultat",
     "print_palsfundament_resultat",
-    "print_trepalsfundament_resultat",
     "trepalsfundament_teoretiskt_innan_slagning",
 ]
